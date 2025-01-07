8600c52c89e57bb6c92e2991822b7cc5af6486c6
Tchoubi11 <tchoubayefranck@gmail.com>
2025-01-07 09:18:24 +0100

    création de userController.php

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+/**
+ * page d'accueil
+ * @return Response
+ * 
+ */
+
+
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        $avis = [
+            "Très belle expérience, mes enfants ont adoré voir les tigres!" => "Cathérine",
+            "Un endroit magnifique et bien entretenu. Bravo à l'équipe!" => "Marcus",
+            "Les habitats sont vraiment bien conçus. Un moment inoubliable." => "John",
+        ];
+
+        $habitats = [
+            'Savane' => ['Lions', 'Girafes', 'Zèbres'],
+            'Forêt Tropicale' => ['Singes', 'Tigres', 'Perroquets'],
+            'Pôle Nord' => ['Ours Polaires', 'Manchots']
+        ];
+
+        $services = [
+            'Cafétérias et restaurants',
+            'Aires de jeux pour enfants',
+            'Boutique de souvenirs',
+            'Visites guidées'
+        ];
+
+        return $this->render('home/index.html.twig', [
+            'reviews' => $avis,
+            'habitats' => $habitats,
+            'services' => $services,
+        ]);
+    }
+}
